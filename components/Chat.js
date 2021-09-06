f49bd2c5081742fd3e43b3cd1ed29b58bb808cba
John Jenkins <jjenksy@gmail.com>
2021-09-06 16:36:28 -0500

    add chat component

--- components/Chat.js
+++ components/Chat.js
@@ -8,17 +8,48 @@ import {
   ListRenderItem,
 } from "react-native";
 import Styles from "./Styles";
-
+import { RenderChatItem } from "./ChatItem";
 
 const Chat = ({ username, image }) => {
   let [chatInput, setChatInput] = useState("");
   let [chatItemList, setChatItemList] = useState([]);
-
+    
+  const renderItem = ({ item }) => (<RenderChatItem chatItem={item} username={username}></RenderChatItem>);
 
   return (
     <View style={Styles.container}>
-          <Text>Hi { username }! You can chat here shortly.</Text>
-
+      <FlatList
+        inverted
+        data={chatItemList.sort((a, b) => b.timeStamp - a.timeStamp)}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      ></FlatList>
+      <View style={Styles.sendSection}>
+        <TextInput
+          style={Styles.chatTextInput}
+          value={chatInput}
+          onChangeText={(text) => {
+            setChatInput(text);
+          }}
+        ></TextInput>
+        <Button
+          title="Send"
+          onPress={() => {
+            setChatItemList([
+              ...chatItemList,
+              {
+                id: Math.random().toString(36).substring(7),
+                text: chatInput,
+                image: image,
+                timestamp: Date.now(),
+                by: username,
+              },
+            ]);
+            console.log(chatItemList);
+            setChatInput("");
+          }}
+        ></Button>
+      </View>
     </View>
   );
 };
